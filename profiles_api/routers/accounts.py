5e12a1ef04c8faa250ffe7395889794ee1f5378c
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2022-07-18 17:43:17 -0500

    hello

--- profiles_api/routers/accounts.py
+++ profiles_api/routers/accounts.py
@@ -126,7 +126,7 @@ async def get_current_user(
 async def get_current_active_user(
     current_user: User = Depends(get_current_user),
 ):
-    # if current_user.disabled:
+    # if current_user.disabled: 
     # raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
